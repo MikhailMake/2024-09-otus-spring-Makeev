445e8c28623238f366a9b468ada14a7436c574f0
MikhailMake <mvmakeev@gmail.com>
2024-10-14 19:41:58 +0300

    HW01

--- hw01/src/main/java/ru/otus/hw/Application.java
+++ hw01/src/main/java/ru/otus/hw/Application.java
@@ -2,10 +2,6 @@ package ru.otus.hw;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.otus.hw.dao.CsvQuestionDao;
-import ru.otus.hw.dao.QuestionDao;
-import ru.otus.hw.service.IOService;
-import ru.otus.hw.service.StreamsIOService;
 import ru.otus.hw.service.TestRunnerService;
 
 public class Application {
