34b83ba276b8beda4a0c60defdd1effe8c299490
mikhailmak <mvmakeev@gmail.com>
2025-01-13 11:12:51 +0400

    hotfix3

--- hw03/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -14,7 +14,7 @@ public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final ResultService resultService;
 
-    private final StreamsIOService ioService;
+    private final LocalizedIOService ioService;
 
     @Override
     public void run() {
@@ -23,7 +23,7 @@ public class TestRunnerServiceImpl implements TestRunnerService {
             var testResult = testService.executeTestFor(student);
             resultService.showResult(testResult);
         } catch (QuestionReadException e) {
-            ioService.printLine("The test questions could not be read");
+            ioService.printLineLocalized("TestRunnerServiceImpl.not.found.file");
         }
 
     }
